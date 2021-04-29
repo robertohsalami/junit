c6af3993b6e72ab585f3837622181b4fb99190c4
Roberto Henrique Salami <robertohsalami@gmail.com>
2021-04-29 16:39:37 -0300

    Testes com Mockito

---
+++ migration/src/test/java/com/example/migration/service/impl/UsuarioServiceImplTest.java
@@ -0,0 +1,73 @@
+package com.example.migration.service.impl;
+
+import com.example.migration.entity.Usuario;
+import com.example.migration.repository.UsuarioRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UsuarioServiceImplTest {
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @InjectMocks
+    private UsuarioServiceImpl usuarioService;
+
+    @Test
+    public void findAllTest(){
+        List<Usuario> usuariosBase = getUsuarios();
+
+        when(usuarioRepository.findAll()).thenReturn(usuariosBase);
+
+        List<Usuario> usuarios = usuarioService.findAll();
+
+        assertEquals(usuarios.get(0).getId().longValue(), 1l);
+        assertEquals(usuarios.get(0).getNome(), "Roberto");
+
+        verify(usuarioRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void saveTest(){
+        Usuario usuarioSaved = getUsuarios().get(0);
+
+        when(usuarioRepository.save(getUsuario())).thenReturn(usuarioSaved);
+
+        Usuario usuario = usuarioService.save(getUsuario());
+
+        assertEquals(usuario.getId().longValue(), 1l);
+        assertEquals(usuario.getCpf(), "23312988820");
+
+        verify(usuarioRepository, times(1)).save(getUsuario());
+    }
+
+    private List<Usuario> getUsuarios() {
+        List<Usuario> usuarios = new ArrayList<>();
+        Usuario usuario1 = new Usuario();
+        usuario1.setId(1l);
+        usuario1.setNome("Roberto");
+        usuario1.setIdade(41l);
+        usuario1.setCpf("23312988820");
+        usuarios.add(usuario1);
+        return usuarios;
+    }
+
+    private Usuario getUsuario(){
+        return Usuario.builder()
+                .nome("Roberto")
+                .idade(41l)
+                .cpf("23312988820")
+                .build();
+    }
+
+}
